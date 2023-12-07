2990e46a3809a9b87e54cb5e6968d94c44098af8
FrostyRobot <davmurray0@hotmail.com>
2023-12-07 00:34:35 +0000

    pushing extra changes

--- AdventOfCodeTests/FirstWeekTests.cs
+++ AdventOfCodeTests/FirstWeekTests.cs
@@ -5,19 +5,31 @@ namespace AdventOfCodeTests
     public class FirstWeekTests
     {
         [Fact]
-        public void Day1_Solve01_Example()
+        public void Day1_Part01_Example()
         {
             Day01 day01 = new Day01("01a");
             Assert.Equal("142", day01.Solve_1());
         }
 
         [Fact]
-        public void Day1_Solve01()
+        public void Day1_Part01_Actual()
         {
             Day01 day01 = new Day01("01");
             Assert.Equal("55386", day01.Solve_1());
         }
 
+        [Fact]
+        public void Day1_Part02_Example()
+        {
+            Day01 day01 = new Day01("01b");
+            Assert.Equal("281", day01.Solve_2());
+        }
 
+        [Fact]
+        public void Day1_Part02_Actual()
+        {
+            Day01 day01 = new Day01("01");
+            //Assert.Equal("55386", day01.Solve_2());
+        }
     }
 }
